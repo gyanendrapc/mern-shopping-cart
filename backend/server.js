7d6f936ea0a42d32b955afcaa88cb18a8731ec43
gyanendrapc <chaudharygyane699@gmail.com>
2021-10-10 23:24:59 +0545

    working on build

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
+// getting dotenv
 require('dotenv').config();
 
+// express configuration
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT;
+
+// connection to mongodb
+const connectDB = require('./config/db');
+connectDB();
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
